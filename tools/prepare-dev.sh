847f2913fc0be8316d61ffd3ca8bf1cf917d8635
Touhid Mia <touhid@banglafighter.com>
2024-04-09 00:06:32 +0600

    Bismillah of Example App

--- tools/prepare-dev.sh
+++ tools/prepare-dev.sh
@@ -3,11 +3,18 @@
 echo "Preparing Development Source Dependency"
 DEPENDENCIES="dependencies"
 mkdir -p "$DEPENDENCIES"
+APPLICATION="application"
+mkdir -p "$APPLICATION"
+
 cd "$DEPENDENCIES"
 
 git clone -b dev https://github.com/banglafighter/react-rapid-app.git
 git clone -b dev https://github.com/banglafighter/react-rapid-bootstrap.git
 git clone -b dev https://github.com/banglafighter/react-rapid-i18n.git
 
-APPLICATION="application"
-mkdir -p "$APPLICATION"
+cd ..
+echo "Installing yarn";
+npm install -g yarn
+
+echo "Installing Dependency";
+yarn install
